052c7cc4cf812363f1b4a13592c50ac17eafb64c
sevda <imanysevda@gmail.com>
2021-04-18 19:59:44 +0430

    complete depthLimited function

--- ids.py
+++ ids.py
@@ -1,17 +1,19 @@
-def iterativeDeepening(graph , startNode,goal):
+import problem
+def iterativeDeepening(graph , start , goal,length , robot = None ):
     depth = 0
     result = None
-    while result == None:
+    while depth < length:
         stack  = []
-        result = depthLimited(graph , startNode,goal ,depth , stack)
+        result = depthLimited(graph , start,goal ,depth , stack  ,robot = robot)
+        if result != None:
+            break
         depth +=1
     return result
     
-def depthLimited(graph , start,goal, depth, stack,  visited = None):
+def depthLimited(graph , start , goal, depth, stack ,visited = None , robot = None):
     stack.append(start)
 
     if start == goal:
-        print(stack)
         return stack
     elif depth == 0 :
         stack.pop() 
@@ -19,14 +21,28 @@ def depthLimited(graph , start,goal, depth, stack,  visited = None):
     
     if visited is None:
         visited = set()
+
     visited.add(start)
     for next in graph.get(start):
         if type(next) == tuple: 
-            if not next[0] in visited:
-                reachGoal =depthLimited(graph , next[0] ,goal, depth-1,stack , visited)
-                if reachGoal:
-                    return reachGoal
+            if (not next[0] in visited )and (problem.checkAvailable(graph , next[0])):
+                if robot != None :
+                    direction =  problem.whichDirection(start , next[0])
+                    if not problem.isDeadlock(start , robot ,iterativeDeepening,direction , graph) :
+                        reachGoal =depthLimited(graph , next[0] ,goal, depth-1,stack, visited, robot)
+                        if reachGoal:
+                             return reachGoal
+                else:
+                    reachGoal =depthLimited(graph , next[0] ,goal, depth-1,stack , visited , robot)
+                    if reachGoal:
+                        return reachGoal
+                
     depth +=1
     stack.pop()
+
+
+
+
+
                 
 
