a59bb2b42a45c82dca2ad42c0e797019cdaf17e1
sevda <imanysevda@gmail.com>
2021-04-18 23:21:10 +0430

    minor changes

--- ids.py
+++ ids.py
@@ -1,16 +1,16 @@
 import problem
-def iterativeDeepening(graph , start , goal,length , robot = None ):
+def iterativeDeepening(graph , start , goal,length,butterCoordinate  = None, robot = None ):
     depth = 0
     result = None
     while depth < length:
         stack  = []
-        result = depthLimited(graph , start,goal ,depth , stack  ,robot = robot)
+        result = depthLimited(graph , start,goal ,depth , stack,butterCoordinate  ,robot = robot)
         if result != None:
             break
         depth +=1
     return result
     
-def depthLimited(graph , start , goal, depth, stack ,visited = None , robot = None):
+def depthLimited(graph , start , goal, depth, stack ,butterCoordinate,visited = None , robot = None):
     stack.append(start)
 
     if start == goal:
@@ -25,15 +25,15 @@ def depthLimited(graph , start , goal, depth, stack ,visited = None , robot = No
     visited.add(start)
     for next in graph.get(start):
         if type(next) == tuple: 
-            if (not next[0] in visited )and (problem.checkAvailable(graph , next[0])):
+            if (not next[0] in visited )and (problem.checkAvailable(graph , next[0], butterCoordinate)):
                 if robot != None :
                     direction =  problem.whichDirection(start , next[0])
-                    if not problem.isDeadlock(start , robot ,iterativeDeepening,direction , graph) :
-                        reachGoal =depthLimited(graph , next[0] ,goal, depth-1,stack, visited, robot)
+                    if not problem.isDeadlock(start , robot ,iterativeDeepening,direction , graph,butterCoordinate) :
+                        reachGoal =depthLimited(graph , next[0] ,goal, depth-1,stack,butterCoordinate, visited, robot)
                         if reachGoal:
                              return reachGoal
                 else:
-                    reachGoal =depthLimited(graph , next[0] ,goal, depth-1,stack , visited , robot)
+                    reachGoal =depthLimited(graph , next[0] ,goal, depth-1,stack ,butterCoordinate, visited , robot)
                     if reachGoal:
                         return reachGoal
                 
