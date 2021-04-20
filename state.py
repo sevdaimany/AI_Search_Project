5cf777251fe784f6c07163abcdcd0997cf33a637
sevda <imanysevda@gmail.com>
2021-04-20 13:52:54 +0430

    add State class to access butters and robot positions

---
+++ state.py
@@ -0,0 +1,30 @@
+class State :
+
+    butters = []
+    robot = None
+    depthIDS = 0
+
+    @classmethod
+    def setRobot(cls ,newPosition):
+        cls.robot = newPosition
+
+    @classmethod
+    def setButter(cls , which , newPosition):
+        cls.butters.insert(which ,newPosition)
+
+    @classmethod
+    def getRobot(cls):
+        return cls.robot
+
+    @classmethod
+    def getButters(cls):
+        return cls.butters
+
+    @classmethod
+    def setDepthIDS(cls , value):
+        cls.depthIDS = value
+
+    @classmethod
+    def getDepthIDS(cls):
+        return cls.depthIDS
+
