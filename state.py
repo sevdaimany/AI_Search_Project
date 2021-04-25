ddafa23a6c522cc1b52fc6a1eae2ceab6ad511e3
sevda <imanysevda@gmail.com>
2021-04-25 14:35:25 +0430

    comment state

--- state.py
+++ state.py
@@ -1,30 +1,30 @@
-class State :
+# class State :
 
-    butters = [0 for i in range(10)]
-    robot = None
-    depthIDS = 0
+#     butters = [0 for i in range(10)]
+#     robot = None
+#     depthIDS = 0
 
-    @classmethod
-    def setRobot(cls ,newPosition):
-        cls.robot = newPosition
+#     @classmethod
+#     def setRobot(cls ,newPosition):
+#         cls.robot = newPosition
 
-    @classmethod
-    def setButter(cls , which , newPosition):
-        cls.butters[which] =newPosition 
+#     @classmethod
+#     def setButter(cls , which , newPosition):
+#         cls.butters[which] =newPosition 
 
-    @classmethod
-    def getRobot(cls):
-        return cls.robot
+#     @classmethod
+#     def getRobot(cls):
+#         return cls.robot
 
-    @classmethod
-    def getButters(cls):
-        return cls.butters
+#     @classmethod
+#     def getButters(cls):
+#         return cls.butters
 
-    @classmethod
-    def setDepthIDS(cls , value):
-        cls.depthIDS = value
+#     @classmethod
+#     def setDepthIDS(cls , value):
+#         cls.depthIDS = value
 
-    @classmethod
-    def getDepthIDS(cls):
-        return cls.depthIDS
+#     @classmethod
+#     def getDepthIDS(cls):
+#         return cls.depthIDS
 
